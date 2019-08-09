c2091edaeff45f4728cb466a524a55e499c84b84
Tim Pornyuenyong <teakpyy@gmail.com>
2019-08-09 18:33:29 -0400

    Added material UI kit and basic template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./NavBar";
+import Markdown from "./Markdown";
+import Blog from "./Blog";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Blog.js
@@ -0,0 +1,268 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Hidden from "@material-ui/core/Hidden";
+import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import Markdown from "./Markdown";
+import post1 from "./blog-post.1.md";
+import post2 from "./blog-post.2.md";
+import post3 from "./blog-post.3.md";
+
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Built with love by the "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </Link>
+      {" team."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    marginBottom: theme.spacing(2)
+  },
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`
+  },
+  toolbarTitle: {
+    flex: 1
+  },
+  toolbarSecondary: {
+    justifyContent: "space-between",
+    overflowX: "auto"
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0
+  },
+  mainFeaturedPost: {
+    position: "relative",
+    height: 500,
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(0),
+    backgroundImage: "url(https://source.unsplash.com/user/erondu)",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center"
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,.3)"
+  },
+  mainFeaturedPostContent: {
+    position: "relative",
+    padding: theme.spacing(3),
+    [theme.breakpoints.up("md")]: {
+      padding: theme.spacing(6),
+      paddingRight: 0
+    }
+  },
+  mainGrid: {
+    marginTop: theme.spacing(3)
+  },
+  card: {
+    display: "flex"
+  },
+  cardDetails: {
+    flex: 1
+  },
+  cardMedia: {
+    width: 160
+  },
+  markdown: {
+    ...theme.typography.body2,
+    padding: theme.spacing(3, 0)
+  },
+  sidebarAboutBox: {
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.grey[200]
+  },
+  sidebarSection: {
+    marginTop: theme.spacing(3)
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0)
+  }
+}));
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description: "MK3 kisses baby, loved by all the younguns"
+  },
+  {
+    title: "Campaign raised 25 million in 3 days",
+    date: "Nov 11",
+    description: "gimme yo money cuz we ballin'"
+  }
+];
+
+const posts = [post1, post2, post3];
+
+const social = ["GitHub", "Twitter", "Facebook"];
+
+export default function Blog() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Toolbar
+          component="nav"
+          variant="dense"
+          className={classes.toolbarSecondary}
+        />
+        <main>
+          {/* Main featured post */}
+          <Paper className={classes.mainFeaturedPost}>
+            {/* Increase the priority of the hero background image */}
+            {
+              <img
+                style={{ display: "none" }}
+                src="https://source.unsplash.com/user/erondu"
+                alt="background"
+              />
+            }
+            <div className={classes.overlay} />
+            <Grid container>
+              <Grid item md={6}>
+                <div className={classes.mainFeaturedPostContent}>
+                  <Typography
+                    component="h1"
+                    variant="h3"
+                    color="inherit"
+                    gutterBottom
+                  >
+                    MK3 for you and me
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                    Join the revolution:
+                  </Typography>
+                  <form className={classes.form}>
+                    <input
+                      className={classes.form}
+                      type="text"
+                      placeholder="First Name"
+                    />
+                    <br />
+                    <input
+                      className={classes.form}
+                      type="text"
+                      placeholder="Last Name"
+                    />
+                    <br />
+                    <input
+                      className={classes.form}
+                      type="email"
+                      placeholder="john.doe@gmail.com"
+                    />
+                    <br />
+                    <input className={classes.form} type="submit" />
+                  </form>
+                </div>
+              </Grid>
+            </Grid>
+          </Paper>
+          {/* End main featured post */}
+          {/* Sub featured posts */}
+          <Grid container spacing={5} className={classes.mainGrid}>
+            {/* Main content */}
+
+            {/* End main content */}
+            {/* Sidebar */}
+            <Grid item xs={12} md={4}>
+              <Paper elevation={0} className={classes.sidebarAboutBox}>
+                <Typography variant="h6" gutterBottom>
+                  About
+                </Typography>
+                <Typography>
+                  Etiam porta sem malesuada magna mollis euismod. Cras mattis
+                  consectetur purus sit amet fermentum. Aenean lacinia bibendum
+                  nulla sed consectetur.
+                </Typography>
+              </Paper>
+            </Grid>
+            {/* End sidebar */}
+          </Grid>
+          <Grid container spacing={1} className={classes.cardGrid}>
+            {featuredPosts.map(post => (
+              <Grid item key={post.title} xs={12} md={6}>
+                <CardActionArea component="a" href="#">
+                  <Card className={classes.card}>
+                    <div className={classes.cardDetails}>
+                      <CardContent>
+                        <Typography component="h2" variant="h5">
+                          {post.title}
+                        </Typography>
+                        <Typography variant="subtitle1" color="textSecondary">
+                          {post.date}
+                        </Typography>
+                        <Typography variant="subtitle1" paragraph>
+                          {post.description}
+                        </Typography>
+                        <Typography variant="subtitle1" color="primary">
+                          Continue reading...
+                        </Typography>
+                      </CardContent>
+                    </div>
+                    <Hidden xsDown>
+                      <CardMedia
+                        className={classes.cardMedia}
+                        image="https://source.unsplash.com/random"
+                        title="Image title"
+                      />
+                    </Hidden>
+                  </Card>
+                </CardActionArea>
+              </Grid>
+            ))}
+          </Grid>
+          {/* End sub featured posts */}
+        </main>
+      </Container>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Container maxWidth="lg">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+          <MadeWithLove />
+        </Container>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
